4591fbaf24d78366f87591cfbfd5d8d4b612efa3
ikhsanmajid <ikhsanmajid@gmail.com>
2023-12-31 17:28:28 +0700

    add space

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -11,12 +11,13 @@ public class Main {
             if (i % 3 == 0 && i % 5 == 0){
                 System.out.print("BIZZBUZZ");
             }else if (i % 3 == 0){
-                System.out.print(" BIZZ ");
+                System.out.print("BIZZ");
             }else if (i % 5 == 0){
-                System.out.print(" BUZZ ");
+                System.out.print("BUZZ");
             }else {
-                System.out.print(" " + i + " ");
+                System.out.print(i);
             }
+            System.out.print(" ");
         }
         System.out.println();
     }
